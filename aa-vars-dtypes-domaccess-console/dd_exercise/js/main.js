6ddf097cf194fc2598b217d33ca815356d930aab
Ahnaf-dev <52428475+Ahnaf-dev@users.noreply.github.com>
2021-01-10 09:58:53 -0700

    Merge branch 'main' of https://github.com/RobbinLawJavaScript/javascript-demos-1

--- aa-vars-dtypes-domaccess-console/dd_exercise/js/main.js
+++ aa-vars-dtypes-domaccess-console/dd_exercise/js/main.js
@@ -2,3 +2,21 @@
 // console.log('h2.intro.innerHTML = ' + intro.innerHTML);
 // intro.innerHTML = 'Introduction';
 // console.log('h2.intro.innerHTML = ' + intro.innerHTML);
+
+let h1 = document.querySelector('h1')
+console.log('h1 =  ' + h1.innerHTML)
+
+let img = document.getElementById("figure-img")
+console.log('image = ' + img.innerHTML)
+
+let p = document.querySelector('p')
+console.log('p =   ' + p.innerHTML)
+
+let h2 = document.querySelector(".intro")
+console.log('h2 =   ' + h2.innerHTML)
+
+var firstName = 'Jane Doe'
+
+var h2Change = document.querySelector(".intro")
+h2Change.innerHTML
+h2Change.innerHTML = 'Introduction'
